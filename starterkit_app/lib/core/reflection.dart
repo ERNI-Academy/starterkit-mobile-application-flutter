776d1e1254f61092c251413fb48577244769b87d
Dustin Catap <dustincatap@gmail.com>
2023-05-17 18:31:06 +0800

    Update

--- starterkit_app/lib/core/reflection.dart
+++ starterkit_app/lib/core/reflection.dart
@@ -2,7 +2,7 @@
 
 @GlobalQuantifyCapability(r'\.\w+ViewModel$', navigatable)
 import 'package:reflectable/reflectable.dart';
-// import 'package:starterkit_app/features/posts/presentation/view_models/post_details_view_model.dart'; // TODO(dustincatap): Check if there is a way to avoid this import when using `reflectable`
+import 'package:starterkit_app/features/posts/presentation/view_models/post_details_view_model.dart'; // TODO(dustincatap): Check if there is a way to avoid this import when using `reflectable`
 
 const DefaultReflector navigatable = DefaultReflector();
 
